64e8382dd767645e1990fbc56ce62a61f53f79b4
jwahn37 <jwahn37@gmail.com>
2017-05-09 14:42:57 +0900

    fin

--- This/.fpga_fnd_driver.ko.cmd
+++
@@ -1,1 +0,0 @@
-cmd_/home/embe-19/Downloads/EmbeHW2/fpga_fnd/fpga_fnd_driver.ko := arm-none-linux-gnueabi-ld -EL -r  -T /work/achroimx_kernel/scripts/module-common.lds --build-id  -o /home/embe-19/Downloads/EmbeHW2/fpga_fnd/fpga_fnd_driver.ko /home/embe-19/Downloads/EmbeHW2/fpga_fnd/fpga_fnd_driver.o /home/embe-19/Downloads/EmbeHW2/fpga_fnd/fpga_fnd_driver.mod.o
--- This/chardev.h
+++
@@ -1,5 +0,0 @@
-#ifndef CHARDEV_H
-#define CHARDEV_H
-
-#include <linux/ioctl.h>
-
